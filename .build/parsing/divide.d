7a2bec5a1d5b6fc7cb1f05271aa0c5348588638f
Anouk Boulore-Vergez <aboulore@3C6.42angouleme.fr>
2024-05-06 15:08:51 +0200

    je veux recup ca

---
+++ .build/parsing/divide.d
@@ -0,0 +1,13 @@
+.build/parsing/divide.o: src/parsing/divide.c headers/minishell.h \
+  lib/libft/headers/libft.h lib/libft/headers/ft_printf.h headers/keys.h \
+  headers/environment.h
+
+headers/minishell.h:
+
+lib/libft/headers/libft.h:
+
+lib/libft/headers/ft_printf.h:
+
+headers/keys.h:
+
+headers/environment.h:
