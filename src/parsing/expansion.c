6e3db6dddd155af5103dfcb0a2d00d7da026cf0c
Anouk Boulore-Vergez <aboulore@3D2.42angouleme.fr>
2024-05-07 15:49:29 +0200

    redir filename, manque indications pour limiter du hd

--- src/parsing/expansion.c
+++ src/parsing/expansion.c
@@ -6,18 +6,29 @@
 /*   By: aboulore <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/02 14:23:07 by aboulore          #+#    #+#             */
-/*   Updated: 2024/05/07 15:26:19 by aboulore         ###   ########.fr       */
+/*   Updated: 2024/05/07 15:47:51 by aboulore         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-/*
-static void	inspect_redir(t_redir_list **redir_list)
+static void	inspect_redir(t_redir_list **redir_list, t_hashtable **env)
 {
-	filename et limiter
+	t_redir_list	*tmp;
+
+	tmp = *redir_list;
+	if (!tmp)
+		return ;
+	while (tmp)
+	{
+		if (tmp->target_filename && ft_strchr(tmp->target_filename, '$'))
+			inspect_token(&tmp->target_filename, env);
+	//if (tmp->open_flags == O_HEREDOC)
+	//faire un signe qui dise de ne pas expand les variables dans le hd
+	//regarder ce qu'a fait arthur
+		tmp = tmp->next;
+	}
 }
-*/
 void	inspect_cmd(t_list **cmd, t_hashtable **env)
 {
 	t_list		*tmp;
@@ -39,9 +50,8 @@ void	expansion(void *item)
 	cmd = (t_command *)item;
 	if (cmd->cmd)
 		inspect_cmd(&cmd->cmd, cmd->env);
-//	if (cmd->redir_list)
-//		inspect_redir(&cmd->redir_list);
-//	MAINTENANT REMOVE QUOTES QUI RESTENT
+	if (cmd->redir_list)
+		inspect_redir(&cmd->redir_list, cmd->env);
 }
 /*
 void	remove_quotes(void *item)
