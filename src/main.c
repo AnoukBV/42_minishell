ef4929b319e92fb1ee8c42da06ab11b30354fc72
Arthur Bernadet <abernade@1C5.42angouleme.fr>
2024-05-15 13:58:26 +0200

    fixed signals behavior while executing

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: abernade <abernade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 10:58:43 by abernade          #+#    #+#             */
-/*   Updated: 2024/05/15 12:35:00 by abernade         ###   ########.fr       */
+/*   Updated: 2024/05/15 13:57:17 by abernade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,6 +67,7 @@ static void	shell_prompt(t_hashtable *env, int ac, char **envp)
 
 	set_rl_signals();
 	line = select_prompt();
+	signals_default();
 	if (!line)
 		return ;
 	pipeline = parsing(line, &tokens, env);
