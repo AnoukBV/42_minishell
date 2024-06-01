d44a1d3a4d89f7049e562faccff479a20dcacd49
Anouk Boulore-Vergez <aboulore@1C5.42angouleme.fr>
2024-06-01 14:01:05 +0200

    tester: syntax et builtins (sauf exit) OK

--- src/parsing/check_syntax.c
+++ src/parsing/check_syntax.c
@@ -6,7 +6,7 @@
 /*   By: aboulore <aboulore@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 18:21:00 by aboulore          #+#    #+#             */
-/*   Updated: 2024/06/01 12:34:26 by aboulore         ###   ########.fr       */
+/*   Updated: 2024/06/01 13:58:59 by aboulore         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 void	syntax_err_prompt(char *token, t_list **inputs)
 {
-	ft_putstr_fd("minishell: syntax error near unexpected token `", 1);
-	ft_putstr_fd(token, 1);
-	ft_putstr_fd("'\n", 1);
+	ft_putstr_fd("minishell: syntax error near unexpected token `", 2);
+	ft_putstr_fd(token, 2);
+	ft_putstr_fd("'\n", 2);
 	if (inputs)
 		ft_lstclear(inputs, &del_wddesc);
 	exit(2);
@@ -43,19 +43,23 @@ void	syntax_errors(t_list **inputs)
 	if (!check)
 		return ;
 	curr = check->content;
-	if ((ft_lstsize(*inputs) == 1 && curr->flags != T_WORD) || \
+	if (curr->flags == T_PIPE)
+		syntax_err_prompt(curr->word, inputs);
+	else if ((ft_lstsize(*inputs) == 1 && curr->flags != T_WORD) || \
 		(ft_lstsize(*inputs) == 2 && (curr->flags == T_RED_IN && \
 		(int)((t_wd_desc *)check->next->content)->flags == T_RED_OUT)))
 		syntax_err_prompt("newline", inputs);
-	if (curr->flags == T_PIPE)
-		syntax_err_prompt(curr->word, inputs);
+	
 	prev = curr;
 	check = check->next;
 	while (check)
 	{
 		curr = check->content;
-		if ((prev->flags != 0 && curr->flags != 0) || (curr->flags != 0 && check->next == NULL))
+		if ((curr->flags == T_PIPE && prev->flags == T_PIPE) || \
+			((prev->flags == T_RED_OUT || prev->flags == T_RED_IN || prev->flags == T_APP_OUT) && curr->flags != T_WORD))
 			syntax_err_prompt(curr->word, inputs);
+		else if (curr->flags != T_WORD && check->next == NULL)
+			syntax_err_prompt("newline", inputs);
 		prev = curr;
 		check = check->next;
 	}
